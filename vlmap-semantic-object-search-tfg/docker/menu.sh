a41db9b7781b5bfe897ad3f056f6e3eada86ca35
Mario <mgarciablasco26@gmail.com>
2026-05-12 16:46:44 +0200

    Enable compact navigation UI

--- vlmap-semantic-object-search-tfg/docker/menu.sh
+++ vlmap-semantic-object-search-tfg/docker/menu.sh
@@ -997,6 +997,8 @@ while true; do
                         echo ""
                         cd /workspace/third_party/vlmaps
                         VLMAPS_ROOM_EXPLORATION=1 \
+                        VLMAPS_UI_COMPACT=1 \
+                        VLMAPS_UI_TILE=1 \
                         VLMAPS_EXPLORE_POINTS="$explore_points_value" \
                         VLMAPS_EXPLORE_POINTS_AUTO="$explore_points_auto" \
                         VLMAPS_EXPLORE_MAX_POINTS="$explore_points_per_ref" \
